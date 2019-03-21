f88129431b23a3b9646f2f5a03051e51275772d2
dondo98 <47148774+dondo98@users.noreply.github.com>
2019-03-21 20:11:01 +0200

    update BitMap file to add updateonDelete method
    updateonDelete is method which update the bitmap on deletion of tuples

--- Bitmap.java
+++ Bitmap.java
@@ -1,8 +1,13 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -46,7 +51,7 @@ public class Bitmap implements Serializable{
 			if(list.size()==maxTuplesPerIndex)
 			{
 				try {
-					FileOutputStream fileOut = new FileOutputStream("bitmaps/"+tableName+colName+"index"+counter++);
+					FileOutputStream fileOut = new FileOutputStream("bitmaps/"+tableName+"_"+colName+"_"+"index"+"_"+counter++);
 					ObjectOutputStream out = new ObjectOutputStream(fileOut);
 					out.writeObject(list);
 					out.close();
@@ -58,10 +63,11 @@ public class Bitmap implements Serializable{
 				list.clear();
 			}
 		}
+		updateMetaBitMap(tableName, colName);
 		if(list.isEmpty())
 			return ; 
 		try {
-			FileOutputStream fileOut = new FileOutputStream("bitmaps/"+tableName+colName+"index"+counter);
+			FileOutputStream fileOut = new FileOutputStream("bitmaps/"+tableName+"_"+colName+"_"+"index"+"_"+counter);
 			ObjectOutputStream out = new ObjectOutputStream(fileOut);
 			out.writeObject(list);
 			out.close();
@@ -72,6 +78,44 @@ public class Bitmap implements Serializable{
 		}
 		
 		
+	}
+	public static void updateMetaBitMap(String tableName,String colName) throws IOException {
+		File meta = new File("meta.csv");
+		ArrayList<String []>newMetaData = new ArrayList();
+		try {
+			Scanner inputStream = new Scanner(meta);
+			while (inputStream.hasNextLine()) {
+				String s = inputStream.nextLine();
+				
+				String []splitted=s.split(", ");
+				if (s.split(", ")[0].equals(tableName) && s.split(", ")[1].equals(colName)) 
+					 splitted[4]="True";
+				newMetaData.add(splitted);		
+				
+				
+			
+		
+			
+		}
+			inputStream.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		File dir = new File("meta.csv");
+		FileWriter fileWriter = new FileWriter(dir);
+
+		BufferedWriter bw = new BufferedWriter(fileWriter);
+		PrintWriter out = new PrintWriter(bw);
+		for(String []line:newMetaData) {
+			for(int i=0;i<line.length;i++)
+			{
+				out.print(line[i]+(i+1==line.length?"":", "));
+			}
+			out.println();
+		}
+		out.flush();
+		out.close();
+		fileWriter.close();
 	}
 	public static ArrayList<String> getUniqueValues(String strTableName, String columnName) throws IOException {
 		ArrayList<String> result = new ArrayList<>();
@@ -128,36 +172,64 @@ public class Bitmap implements Serializable{
 		}
 		return -1;
 	}
+	// assuming parameters are 1-indexed
+	public static int getBitPosition(int pageNumber,int posInPage) {
+		return (pageNumber-1) * DBApp.maxTuplesPerPage + posInPage -1;
+		
+	}
+	public static void updateOnDelete(int pageNumber,int posInPage, String tableName ) throws IOException
+	{
+		System.out.println(posInPage+"  "+pageNumber);
+		int start=pageNumber * DBApp.maxTuplesPerPage ,end = start+ DBApp.maxTuplesPerPage;
+		System.out.println(start+"  "+end);
+		String[] paths = new File("bitmaps").list();
+		int location = pageNumber * DBApp.maxTuplesPerPage + posInPage;
+		for(String path:paths)
+		{
+			String []splitted=path.split("_");
+			if(splitted[0].equals(tableName)) {
+				ArrayList<BitmapPair> list = getBitMapPair("bitmaps/"+path);
+				for(BitmapPair pair:list) {
+					String bitmap = pair.bitmap;
+					String before=bitmap.substring(0,start), after = bitmap.substring(end,bitmap.length());
+					String x = bitmap.substring(start,location), y =bitmap.substring(location+1,end);
+					String s= before+x+y+"0"+after;
+					pair.bitmap=s;
+					System.out.println(pair.value+". before:"+bitmap +". afterDeletion:"+pair.bitmap);
+					
 
-//	public static void mapCreation(String tableName, String columnName) throws IOException {
-//		if (ExistTable(tableName)) {
-//			ArrayList<String> x = getUniqueValues(tableName, columnName);
-//			int y = getIndexofColumn(tableName, columnName);
-//			File file = new File("pages/");
-//			String[] paths = file.list();
-//			paths = sortPaths(paths);
-//			// looping on unique values
-//			for (int i = 0; i < x.size(); i++) {
-//				//looping on every path
-//				for (int j = 0; j < paths.length; j++) {
-//					String[] n = paths[j].split("_");
-//					//if path start with the tableName will search for the value of unique value of loop i
-//					if(n[0].equals(tableName)) {
-//						Vector<Object> v = getNumberOfTuples("pages/" + paths[j]);
-//						for(int k=0;k<v.size();k++) {
-//							Object[] oneRow = (Object[]) v.get(k);
-//							if(oneRow[y].equals(x.get(i))) {
-//								// should be add into the bit map  and the page number will be n[1]
-//							}
-//						}
-//						
-//					}
-//				}
-//
-//			}
-//
-//		}
-//	}
+					
+				}
+				FileOutputStream fileOut = new FileOutputStream("bitmaps/"+path);
+				ObjectOutputStream out = new ObjectOutputStream(fileOut);
+				out.writeObject(list);
+				out.close();
+				fileOut.close();
+			}
+		}
+	}
+
+	
+	
+	static ArrayList<BitmapPair> getBitMapPair(String className) {
+		ArrayList<BitmapPair> v;
+		try {
+			FileInputStream fileIn = new FileInputStream(className);
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			v = (ArrayList<BitmapPair>) in.readObject();
+			in.close();
+			fileIn.close();
+			return v;
+		} catch (IOException i) {
+			i.printStackTrace();
+			return null;
+		} catch (ClassNotFoundException c) {
+			System.out.println("class not found");
+			c.printStackTrace();
+			return null;
+		}
+	}
+
 
 	
 }
